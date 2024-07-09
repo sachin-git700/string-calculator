af92090b7ee8784aecb9cb656e9e612d6be80a57
sachin-git700 <sachin.git700@gmail.com>
2024-07-09 22:23:54 +0530

    fixes rubocop offenses

--- main.rb
+++ main.rb
@@ -1,5 +1,8 @@
 # frozen_string_literal: true
 
+# The StringCalculator class provides a method to sum numbers given in a string format.
+# It supports different delimiters, handles new lines between numbers,
+# and raises exceptions for negative numbers.
 class StringCalculator
   def self.add(numbers)
     return 0 if numbers.empty?
